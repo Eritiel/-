0f49376a54321632a44070dff8f40feac8db9e54
Eritiel <Luna107@bk.ru>
2020-05-19 19:06:56 +0300

    Update ind_zad_2.pl

--- ind_zad_2.pl
+++ ind_zad_2.pl
@@ -2,14 +2,22 @@ read_list(A,N):-rl([],A,0,N).
 rl(A,A,N,N):-!.
 rl(B,A,I,N):-I1 is I+1,read(X), append(B,[X],B1), rl(B1,A,I1,N).
 
-bubblesort(InputList,SortList) :-
-    swap(InputList,List), ! , bubblesort(List,SortList).
-bubblesort(SortList,SortList).
+move_max_to_end([], []):-!.
+move_max_to_end([Head], [Head]):-!.
+move_max_to_end([First, Second|Tail], [Second|ListWithMaxEnd]):-
+  First > Second, !,
+  move_max_to_end([First|Tail], ListWithMaxEnd).
+move_max_to_end([First, Second|Tail], [First|ListWithMaxEnd]):-
+  move_max_to_end([Second|Tail], ListWithMaxEnd).
 
-swap([X,Y|List],[Y,X|List]) :- X > Y.
-swap([Z|List],[Z|List1]) :- swap(List,List1).
+bubble_sort(SortedList, SortedList):-
+  move_max_to_end(SortedList, DoubleSortedList),
+  SortedList = DoubleSortedList, !.
+bubble_sort(List, SortedList):-
+  move_max_to_end(List, SortedPart),
+  bubble_sort(SortedPart, SortedList).
 
 write_list([X]):-write(X),!.
 write_list([X|Y]):-write(X),write_list(Y).
 
-?-read(N), read_list(A,N), bubblesort(A, B), write_list(B).
+?-read(N), read_list(A,N), bubble_sort(A, B), write_list(B).
