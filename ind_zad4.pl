56ac5b76b6f82745e3df58976c8e11bacaacc973
Eritiel <Luna107@bk.ru>
2020-05-20 00:29:59 +0300

    Update ind_zad4.pl

--- ind_zad4.pl
+++ ind_zad4.pl
@@ -1,6 +1,98 @@
-
 in_list([El|_],El).
 in_list([_|T],El):-in_list(T,El).
 
 write_list([X]):-write(X),!.
 write_list([X|Y]):-write(X),write_list(Y).
+
+List=[_],
+(
+  (
+    (   in_list(List,[vitya]); in_list(List,[kolya])),
+
+    (   in_list(List,[kolya]); in_list(List,[andrew]);in_list(List,[dima])),
+
+    (
+     (  (   in_list(List,[vitya]); in_list(List,[kolya])),
+        (   in_list(List,[vitya]); in_list(List,[jura])));
+
+     (  (   in_list(List,[andrew]); in_list(List,[dima]);in_list(List,[jura])),
+        (   in_list(List,[kolya]); in_list(List,[andrew]);in_list(List,[dima])))),
+
+    (
+     (  (   in_list(List,[vitya]); in_list(List,[kolya])),
+        (   in_list(List,[kolya]); in_list(List,[andrew]);in_list(List,[dima])));
+     (  (   in_list(List,[andrew]); in_list(List,[dima]);in_list(List,[jura])),
+        (   in_list(List,[vitya]); in_list(List,[jura])))));
+
+
+   (
+    (   in_list(List,[andrew]); in_list(List,[dima]);in_list(List,[jura])),
+
+    (   in_list(List,[kolya]); in_list(List,[andrew]);in_list(List,[dima])),
+
+    (
+     (  (   in_list(List,[vitya]); in_list(List,[kolya])),
+        (   in_list(List,[vitya]); in_list(List,[jura])));
+
+     (  (   in_list(List,[andrew]); in_list(List,[dima]);in_list(List,[jura])),
+        (   in_list(List,[kolya]); in_list(List,[andrew]);in_list(List,[dima])))),
+
+    (
+     (  (   in_list(List,[vitya]); in_list(List,[kolya])),
+        (   in_list(List,[kolya]); in_list(List,[andrew]);in_list(List,[dima])));
+     (  (   in_list(List,[andrew]); in_list(List,[dima]);in_list(List,[jura])),
+        (   in_list(List,[vitya]); in_list(List,[jura])))));
+
+
+  (
+    (   in_list(List,[vitya]); in_list(List,[kolya])),
+
+    (   in_list(List,[vitya]); in_list(List,[jura])),
+
+    (
+     (  (   in_list(List,[vitya]); in_list(List,[kolya])),
+        (   in_list(List,[vitya]); in_list(List,[jura])));
+
+     (  (   in_list(List,[andrew]); in_list(List,[dima]);in_list(List,[jura])),
+        (   in_list(List,[kolya]); in_list(List,[andrew]);in_list(List,[dima])))),
+
+    (
+     (  (   in_list(List,[vitya]); in_list(List,[kolya])),
+        (   in_list(List,[kolya]); in_list(List,[andrew]);in_list(List,[dima])));
+     (  (   in_list(List,[andrew]); in_list(List,[dima]);in_list(List,[jura])),
+        (   in_list(List,[vitya]); in_list(List,[jura])))));
+
+
+
+
+
+  (
+    (   in_list(List,[vitya]); in_list(List,[kolya])),
+
+    (   in_list(List,[kolya]); in_list(List,[andrew]);in_list(List,[dima])),
+
+    (
+     (  (   in_list(List,[vitya]); in_list(List,[kolya])),
+        (   in_list(List,[vitya]); in_list(List,[jura])));
+
+     (  (   in_list(List,[andrew]); in_list(List,[dima]);in_list(List,[jura])),
+        (   in_list(List,[kolya]); in_list(List,[andrew]);in_list(List,[dima])))));
+
+
+
+
+
+  (
+    (   in_list(List,[vitya]); in_list(List,[kolya])),
+
+    (   in_list(List,[kolya]); in_list(List,[andrew]);in_list(List,[dima])),
+
+    (
+     (  (   in_list(List,[vitya]); in_list(List,[kolya])),
+        (   in_list(List,[kolya]); in_list(List,[andrew]);in_list(List,[dima])));
+     (  (   in_list(List,[andrew]); in_list(List,[dima]);in_list(List,[jura])),
+        (   in_list(List,[vitya]); in_list(List,[jura])))))),
+
+
+
+ write_list(List).
