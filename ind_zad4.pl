7c127e5290b32abfb7d78802943c2bf9c07623bb
Eritiel <Luna107@bk.ru>
2020-05-20 00:41:20 +0300

    Update ind_zad4.pl

--- ind_zad4.pl
+++ ind_zad4.pl
@@ -4,6 +4,7 @@ in_list([_|T],El):-in_list(T,El).
 write_list([X]):-write(X),!.
 write_list([X|Y]):-write(X),write_list(Y).
 
+
 List=[_],
 (
   (
@@ -23,7 +24,6 @@ List=[_],
         (   in_list(List,[kolya]); in_list(List,[andrew]);in_list(List,[dima])));
      (  (   in_list(List,[andrew]); in_list(List,[dima]);in_list(List,[jura])),
         (   in_list(List,[vitya]); in_list(List,[jura])))));
-
 
    (
     (   in_list(List,[andrew]); in_list(List,[dima]);in_list(List,[jura])),
@@ -62,10 +62,6 @@ List=[_],
      (  (   in_list(List,[andrew]); in_list(List,[dima]);in_list(List,[jura])),
         (   in_list(List,[vitya]); in_list(List,[jura])))));
 
-
-
-
-
   (
     (   in_list(List,[vitya]); in_list(List,[kolya])),
 
@@ -78,10 +74,6 @@ List=[_],
      (  (   in_list(List,[andrew]); in_list(List,[dima]);in_list(List,[jura])),
         (   in_list(List,[kolya]); in_list(List,[andrew]);in_list(List,[dima])))));
 
-
-
-
-
   (
     (   in_list(List,[vitya]); in_list(List,[kolya])),
 
@@ -92,7 +84,5 @@ List=[_],
         (   in_list(List,[kolya]); in_list(List,[andrew]);in_list(List,[dima])));
      (  (   in_list(List,[andrew]); in_list(List,[dima]);in_list(List,[jura])),
         (   in_list(List,[vitya]); in_list(List,[jura])))))),
-
-
 
  write_list(List).
